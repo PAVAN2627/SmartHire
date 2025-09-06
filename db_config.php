3572f5580376264597e5704c4fdee0c74fd18feb
Pavanmali0281 <166298170+PAVAN2627@users.noreply.github.com>
2025-09-07 02:17:55 +0530

    Updated

--- db_config.php
+++ db_config.php
@@ -3,7 +3,6 @@ $host = "localhost";
 $dbname = "smarthire";
 $user = "root";       // default in XAMPP
 $password = "";       // leave blank if you're using XAMPP
-
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
